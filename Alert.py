1241094f05df6f7e7d6f1ddd55b6e1aa93fc2008
AIRICLEE <49914236+AIRICLEE@users.noreply.github.com>
2020-03-02 17:48:36 -0500

    Update Alert.py

--- Alert.py
+++ Alert.py
@@ -1,2 +1,12 @@
+import AI
 
 #test01
+def alert(ProcessedData):
+
+  #   Call for API from AI module
+  standardData = AI.standarData
+  if(ProcessedData > standardData):
+    return true
+  else:
+    return false
+ 
