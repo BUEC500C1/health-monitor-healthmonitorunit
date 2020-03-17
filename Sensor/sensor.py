c16f2787c0e08a2e8ab11d2601a2377f415936f6
E1visz <55214592+E1visz@users.noreply.github.com>
2020-03-16 20:36:17 -0400

    Update sensor.py

--- Sensor/sensor.py
+++ Sensor/sensor.py
@@ -1,4 +1,13 @@
-import time
 import random
 
 def generatedata():
+#   generate fake values
+  heartrate = random.randint(60,100)
+  bloodpressure = [random.randint(60,90),random.randint(90,140)]
+  
+  dict = {}
+  dict['heartrate'] = heartrate
+  dict['bloodpressure'] = bloodpressure
+  
+  return dict
+  
