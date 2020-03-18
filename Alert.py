3d43b2b609dff6b85ecbaf1d4eece7405f9ca778
AIRICLEE <49914236+AIRICLEE@users.noreply.github.com>
2020-03-17 23:55:23 -0400

    Add func explain to code

--- Alert.py
+++ Alert.py
@@ -1,3 +1,7 @@
+# The input of this func is processed data from func processor()
+# The output is the flag to alert by true or false. True means alert, False means as usual
+# The func is to compare the processed data with standard data we get from AI module, and relatively to alert or not
+
 import AI
 
 #test01
