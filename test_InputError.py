4a0f2f6a111c8221a76ca6ca8e42110d7ba68a6c
JWangNov <wangjialun@126.com>
2020-03-18 17:55:42 -0400

    Update test_InputError.py

--- test_InputError.py
+++ test_InputError.py
@@ -2,10 +2,9 @@ import pytest
 from InputError import InputError
 
 
-class TestAssert():
-    def test_raise_exception(self):
-        with pytest.raises(InputError) as excinfo:
-            raise InputError("Nyet!!")
+def test_raise_exception():
+    with pytest.raises(InputError) as excinfo:
+        raise InputError("Nyet!!")
 
-        assert "Nyet!!" in str(excinfo)
-        assert excinfo.type == InputError
+    assert "Nyet!!" in str(excinfo)
+    assert excinfo.type == InputError
