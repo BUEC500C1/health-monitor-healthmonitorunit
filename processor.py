9e205e21968685925a901d3dd8b8b40704165008
hanchenzhang <43228680+hanchenzhang@users.noreply.github.com>
2020-03-18 00:13:25 -0400

    Update processor.py

--- processor.py
+++ processor.py
@@ -5,3 +5,15 @@ def sensor_unit():
 
 def processor():
   return 0
+
+import AI
+
+#test01
+def alert(ProcessedData):
+
+  #   Call for API from AI module
+  standardData = AI.standarData
+  if(ProcessedData > standardData):
+    return true
+  else:
+    return false
