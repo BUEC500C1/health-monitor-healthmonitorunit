62bff007df025a1e3bb2dcfe8b6226274433ba86
hanchenzhang <43228680+hanchenzhang@users.noreply.github.com>
2020-03-18 00:18:33 -0400

    Update processor.py

--- processor.py
+++ processor.py
@@ -5,7 +5,18 @@ def sensor_unit():
 
 def processor():
   return 0
-
+######################
+def generatedata():
+#   generate fake values
+  heartrate = random.randint(60,100)
+  bloodpressure = [random.randint(60,90),random.randint(90,140)]
+  
+  dict = {}
+  dict['heartrate'] = heartrate
+  dict['bloodpressure'] = bloodpressure
+  
+  return dict
+##################
 import AI
 
 #test01
@@ -17,3 +28,24 @@ def alert(ProcessedData):
     return true
   else:
     return false
+  
+#below are just draft saved since it might be easy to work when offline
+######################
+  def ai(impulse, blood_pressure, blood_oxygen, nn):
+  # get latest nn groups of input from database
+  # if nn == null or nn <= 0 or any one of input is null:
+  # . throw WRONG_INPUT
+  
+  # make some analysis
+  
+  # may raise alert..
+  
+  # return future['impulse', 'blood_pressure', 'blood_oxygen']
+  
+  pass
+#############################
+import random
+
+def monitor(AI_data, flag=null):
+  
+def show(res):
