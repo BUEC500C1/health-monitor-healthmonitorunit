2705c7b2ba7b1831daee3968fb81ed623e728849
chijunyou <55154341+chijunyou@users.noreply.github.com>
2020-04-22 06:47:17 -0400

    Update database.py

--- database.py
+++ database.py
@@ -6,14 +6,22 @@ class database(object):
 	def __init__(self):
 		self.data = list()
 		return self;
+	def __str__(self):
+		return "database has" str(len(self.data)) + "pieces data"
 
 	def getdata(self,number):
 		length = len(self.data)
 		if length<number:
 			print("do not have enough data for input number")
-			raise NumberErro
+			raise NumberError
 		else:
 			return self.data[length-number:length]
+		
+	def getdatabytime(self,time):
+		for d in self.data:
+			if d[0] == time:
+				return d
+		return None
 
 	def save(self,time,impulse,oxygen):
 		self.data.append((time,impulse,oxygen))
@@ -22,7 +30,7 @@ class database(object):
 		length = len(self.data)
 		if length<number:
 			print("do not have enough data to delete")
-			raise NumberErro
+			raise NumberError
 		else:
 			temp = self.data[0:length-number]
 			self.data = temp
@@ -30,7 +38,7 @@ class database(object):
 		length = len(self.data)
 		if length<number:
 			print("do not have enough data to delete")
-			raise NumberErro
+			raise NumberError
 		else:
 			temp = self.data[number:length]
 			self.data = temp
