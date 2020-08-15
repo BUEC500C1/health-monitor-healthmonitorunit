44e898bc3eb2027d91481302ca3b945a45ed9916
hanchenzhang <43228680+hanchenzhang@users.noreply.github.com>
2020-08-15 09:51:42 +0800

    Update processor.py

--- processor.py
+++ processor.py
@@ -1,51 +1,58 @@
 from sensor import generatedata
+from database import database
+from Alert import alert, firstAlert, laterWarning 
+from Monitor import Monitor
+from AI_Analysis import AI_Analysis
+import time
+from termcolor import colored
+def pulseSimulation():
+	time.sleep(1)
+	return generatedata(2)
 
-def sensor_unit():
-  return 0
+def Processing(patientRecord):
+	AAA = firstAlert()
+	BBB = laterWarning()
+	AAA.check(patientRecord.getdata(1)[0])
+	BBB.check(patientRecord.getdata(1)[0])
+	if AAA.message == "":
+		if BBB.message != "":
+			print(colored(BBB, 'yellow'))
+		print(colored("Patient Heart Currently Runs Normally😊",'green')) 
+	else:
+		print(colored(AAA, 'red'))
+	return patientRecord.getdata(1)[0]
 
-def processor():
-  return 0
-######################
-def generatedata():
-#   generate fake values
-  heartrate = random.randint(60,100)
-  bloodpressure = [random.randint(60,90),random.randint(90,140)]
-  
-  dict = {}
-  dict['heartrate'] = heartrate
-  dict['bloodpressure'] = bloodpressure
-  
-  return dict
-##################
-import AI
 
-#test01
-def alert(ProcessedData):
+def AI(patientRecord, n):
+	if len(patientRecord.data) < n:
+		return patientRecord.getdata(1)[0]
+	historicalSample = patientRecord.getdata(n)
+	obj = AI_Analysis()
+	tip = []
+	tbp = []
+	tbo = []
+	for samp in historicalSample:
+		tip.append(samp[1])
+		tbp.append(samp[2])
+		tbo.append(samp[3])
+	out = obj.analysis(tip, tbp, tbo, n)
+	return out
 
-  #   Call for API from AI module
-  standardData = AI.standarData
-  if(ProcessedData > standardData):
-    return true
-  else:
-    return false
-  
-#below are just draft saved since it might be easy to work when offline
-######################
-  def ai(impulse, blood_pressure, blood_oxygen, nn):
-  # get latest nn groups of input from database
-  # if nn == null or nn <= 0 or any one of input is null:
-  # . throw WRONG_INPUT
-  
-  # make some analysis
-  
-  # may raise alert..
-  
-  # return future['impulse', 'blood_pressure', 'blood_oxygen']
-  
-  pass
-#############################
-import random
 
-def monitor(AI_data, flag=null):
-  
-def show(res):
+
+if __name__ == '__main__':
+	measurementTime = 100
+	currentTime = 0
+	patientRecord = database()
+	#processedRecord = database()
+	#AIRecord = database()
+	while (currentTime < measurementTime):
+		dictRecord = pulseSimulation()
+		impulse = dictRecord['heartrate']
+		bPressure = dictRecord['bloodpressure']
+		bOxygen = dictRecord['bloodoxygen']
+		patientRecord.save(currentTime, impulse, bPressure,bOxygen)
+		Mo = Monitor(Processing(patientRecord),AI(patientRecord,4))
+		if (currentTime % 4 == 0):
+			Mo.show()
+		currentTime += 1
